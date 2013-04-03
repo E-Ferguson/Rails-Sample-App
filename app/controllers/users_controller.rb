e6405fca3164fe2e91abfcbef7f4474015a3702c
Eddie Ferguson <erferguson@email.wm.edu>
2013-04-03 18:20:34 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
-  def new
+
+  def show #used to show a users page
+  	@user = User.find(params[:id])
   end
+
+  def new 
+  	@user = User.new
+  end
+  
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save   #if the form is valid
+  		flash[:success] = "Welcome to the vagina_boo!"
+  		redirect_to @user
+  	else
+  		render 'new' # that is, go to new.html.erb
+  	end
+  end
+  
 end
